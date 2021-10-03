be598e79f049b8140e70d374b597ae95047b3739
xiuxiu62 <jacremer@live.com>
2021-10-03 16:01:12 -0700

    patch window gaps

--- config.h
+++ config.h
@@ -2,9 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx = 0; /* border pixel of windows */
+static const unsigned int gappx = 8;    /* gap pixel between windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 1;            /* 0 means bottom bar */
+static const Bool showbar = 1;          /* 0 means no bar */
+static const Bool topbar = 1;           /* 0 means bottom bar */
+
 static const char *fonts[] = {"iosevka:size=10"};
 static const char dmenufont[] = "iosevka:size=10";
 static const char col_gray1[] = "#222222";
